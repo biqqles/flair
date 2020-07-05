8d8ebabe65811c24432f64e7fa6ac0c3574cfe3b
biqqles <biqqles@protonmail.com>
2020-07-05 22:22:28 +0100

    Process: avoid crashing due to int/handle confusion

--- flair/hook/process.py
+++ flair/hook/process.py
@@ -5,17 +5,13 @@
  License, v. 2.0. If a copy of the MPL was not distributed with this
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
-import win32con
-
-from .. import IS_WIN
-
 from typing import Tuple
-from pywintypes import HANDLE
-
 from ctypes import *
-if IS_WIN:
-    import win32api
-    import win32process
+
+from pywintypes import HANDLE
+import win32api
+import win32process
+import win32con
 
 from .window import get_hwnd
 
@@ -49,7 +45,7 @@ def get_process() -> HANDLE:
     return process
 
 
-def read_memory(process, address, datatype, buffer_size=128):
+def read_memory(process: HANDLE, address: int, datatype: type, buffer_size=128):
     """Reads Freelancer's process memory.
 
     Just as with string resources, strings are stored as UTF-16 meaning that the end of a string is marked by two
@@ -58,8 +54,10 @@ def read_memory(process, address, datatype, buffer_size=128):
     """
     buffer = create_string_buffer(buffer_size)
     value = datatype()
-    if windll.kernel32.ReadProcessMemory(process.handle, address, buffer, len(buffer), 0):
-        if datatype == str:
+    handle = process if isinstance(process, int) else process.handle
+
+    if windll.kernel32.ReadProcessMemory(handle, address, buffer, len(buffer), 0):
+        if datatype is str:
             value = ''.join(map(chr, buffer.raw[:buffer.raw.index(b'\0\0'):2]))
         else:
             memmove(byref(value), buffer, sizeof(value))
@@ -67,33 +65,33 @@ def read_memory(process, address, datatype, buffer_size=128):
     return value
 
 
-def get_value(process, key, size=None):
+def get_value(process: HANDLE, key, size=None):
     """Read a value from memory. `key` refers to the key of an address in `READ_ADDRESSES`"""
     address, datatype = READ_ADDRESSES[key]
     return read_memory(process, address, datatype, buffer_size=size or (sizeof(datatype) * 8))
 
 
-def get_string(process, key, length):
+def get_string(process: HANDLE, key, length):
     """Read a UTF-16 string from memory."""
     return get_value(process, key, (length * 2) + 2)
 
 
-def get_name(process) -> str:
+def get_name(process: HANDLE) -> str:
     """Read the name of the active character from memory."""
     return get_string(process, 'name', 23)
 
 
-def get_credits(process) -> int:
+def get_credits(process: HANDLE) -> int:
     """Read the credit balance of the active character from memory."""
     return get_value(process, 'credits')
 
 
-def get_position(process) -> Tuple[float, float, float]:
+def get_position(process: HANDLE) -> Tuple[float, float, float]:
     """Read the position of the active character from memory."""
     return get_value(process, 'pos_x'), get_value(process, 'pos_y'), get_value(process, 'pos_z')
 
 
-def get_mouseover(process) -> str:
+def get_mouseover(process: HANDLE) -> str:
     """This is a really interesting address. It seems to store random, unconnected pieces of text that have been
     recently displayed or interacted with in the game. These range from console outputs to the names of bases
     and planets immediately upon jumping in or docking, to the prices of commodities in the trader screen, to
@@ -102,28 +100,28 @@ def get_mouseover(process) -> str:
     return get_string(process, 'mouseover', 128)
 
 
-def get_rollover(process) -> str:
+def get_rollover(process: HANDLE) -> str:
     """Similar to mouseover, but usually contains tooltip text."""
     return get_string(process, 'rollover', 128)
 
 
-def get_last_message(process) -> str:
+def get_last_message(process: HANDLE) -> str:
     """Read the last message sent by the player from memory"""
     return get_string(process, 'last_message', 127)
 
 
-def get_chat_box_state(process) -> bool:
+def get_chat_box_state(process: HANDLE) -> bool:
     """Read the state of the chat box from memory."""
     dialogue_hooking_enter = get_value(process, 'enter_dialogue')
     dialogue_focused = get_value(process, 'focus_dialogue')
     return bool(dialogue_hooking_enter and not dialogue_focused)
 
 
-def get_character_loaded(process) -> bool:
+def get_character_loaded(process: HANDLE) -> bool:
     """Read whether a character is loaded (whether in SP or MP)."""
     return bool(get_value(process, 'logged_in'))
 
 
-def get_docked(process) -> bool:
+def get_docked(process: HANDLE) -> bool:
     """Read whether the active character is docked."""
     return not bool(get_value(process, 'in_space'))
